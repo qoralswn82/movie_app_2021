7347d2897210058e1dcf8c0f207c3374330fb0f7
qoralswn82 <58283656+qoralswn82@users.noreply.github.com>
2021-04-22 00:54:08 +0900

    complete to make Movie app add css deploy page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import "./App.css";
 
 
 class App extends React.Component{
@@ -22,18 +23,20 @@ class App extends React.Component{
   }
   render() {
     const {isLoading, movies} = this.state;
-    return <section class="container">
+    return <section className="container">
         {isLoading ? 
-        (<div class="loader">
-            <span class="loader_text">Loading...</span>
-        </div> ): 
-        (<div class="movies">
+        (<div className="loader">
+            <span className="loader_text">Loading...</span>
+        </div> )
+        : 
+        (<div className="movies">
           {
           movies.map(movie =>(
-           <Movie id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />;
-          ));
+           <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres}/>
+          ))
           }
-        </div>)}
+        </div>
+        )}
     </section>
   }
 }
