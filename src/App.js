643fa0767b4541b8e9afa6414c0c16d5a47bfa4e
baekminju <bmj@jkds.kr>
2021-04-21 17:04:52 +0900

    #3.2 Component Life Cycle and State state value set and change function according to Life Cycle componentDidMount componentDidUpdate componentWillUnmount Constructor : when the class is made, the constructor call

--- src/App.js
+++ src/App.js
@@ -1,72 +1,57 @@
 import React from 'react';
-import PropTypes from "prop-types";
 
 
-
-
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-      rating : 5
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.8
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 5.5
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.7
-  }
-];
-
-function Food({name, img,rating}) {
-  return <div>
-      <h1>I like {name}</h1>
-      <h1>{rating}/5.0</h1>
-      <img src={img} alt={name}></img>
-  </div>;
-}
-
-//props 타입을 정의 (의도하지 않은 값의 타입을 걸러내기 위해서 설정)
-Food.propTypes={
-  name : PropTypes.string.isRequired,
-  img : PropTypes.string.isRequired,
-  rating : PropTypes.number
+class App extends React.Component{
+ 
+  //생성자 : 클래스가 생성 되면서 제일 먼저 실행
+ constructor(props){
+   super(props);
+  console.log("생성자");
+ }
+  //변하는 데이터 state
+  state = {
+      count : 0
+  };
+  // state를 변경할 때마다(즉 setState() 함수를 호출 했을 때) render()함수 재호출 새로운 화면 그림(변경된 state 값만을 재렌더링)
+add=()=>{
+  this.setState(current =>({
+    count : current.count +1
+  }))
 };
-
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      {foodILike.map(dish=>{return <Food key={dish.id} name={dish.name} img = {dish.image} rating = {dish.rating}/>})}
-    </div>
-  );
+// this.state를 사용하는 것 보다 current param를 받아서 현재의 state 값을 가져오고 변경하는 것을 추천
+minus=()=>{
+  this.setState(current=>({
+    count : current.count -1
+  }))
+};
+//컴포넌트 들이 렌더링 된 후(처음 화면이 렌더링 된 후 )
+componentDidMount(){
+  console.log("component rendered");
 }
+
+//add나 minus 함수로 update된 후(render 함수 실행 후 실행됨) 
+componentDidUpdate(){
+  console.log("I  just updated");
+}
+
+//컴포넌트 죽을때 (ex 다른 페이지로 이동)
+componentWillUnmount(){
+  console.log("die component");
+}
+  render() {
+    console.log("rendering");
+    return <div>
+        <h1>The number is :  {this.state.count}</h1>
+        {/* this.add()를 사용하지 않는 이유
+          즉시 함수를 호출하시 않고 버튼을 클릭할 때만 호출하기 위해서
+        */}
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+
+    </div>
+  }
+}
+
+
 
 export default App;
